8733695697e875ebb6d981437835e7413389afe3
Ishaan Kashyap <88713875+IshaanKetchup@users.noreply.github.com>
2022-07-24 18:05:28 +0530

    Update google.py

--- cogs/google.py
+++ cogs/google.py
@@ -1,10 +1,5 @@
 from discord.ext import commands
 import discord
-import random
-import bs4
-from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen
-from discord import Embed
 
 class Google(commands.Cog):
 
@@ -26,6 +21,19 @@ class Google(commands.Cog):
                 y = x
             finst += y
         await ctx.reply('https://www.google.com/search?q='+ finst)
+    
+    @commands.command(aliases = ['yt'])
+    async def youtube(self, ctx):
+        squery = str(ctx.message.content)
+        finst = ''
+        for x in (squery.split()[1::]):
+            if x != squery.split()[-1]:
+                y = x + '+'
+            else:
+                y = x
+            finst += y
+        await ctx.reply('https://www.youtube.com/results?search_query='+ finst)
+    
     
     
     
