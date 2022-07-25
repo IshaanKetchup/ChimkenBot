34a6c7eda3bfddbc396d2ba87b2d806c1b951bfc
Ishaan Kashyap <88713875+IshaanKetchup@users.noreply.github.com>
2022-07-25 20:46:49 +0530

    Update bucks.py

--- cogs/bucks.py
+++ cogs/bucks.py
@@ -418,7 +418,7 @@ class BucksDB(commands.Cog):
     @commands.command()
     async def gamble(self, ctx, message = None):
         await ctx.send('Gambling')
-        '''users = self.users
+        users = self.users
         if ctx.author.id in users:
             if message is not None:
                 cash = self.checkcash
@@ -1022,7 +1022,7 @@ class BucksDB(commands.Cog):
                 message = await ctx.send(embed = emb)
         else:
             emb = Embed(title = 'Welcome to ChimkenBucks!', description = 'You don\'t have a record. Type `>start` to begin!.')
-            await ctx.reply(embed = emb)'''
+            await ctx.reply(embed = emb)
 
 
 
